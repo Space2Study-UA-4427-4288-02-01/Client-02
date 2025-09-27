12cec771c8945b73cce9f341e227f495bb4f7f37
4Rinzler4 <127138464+4Rinzler4@users.noreply.github.com>
2025-09-28 00:30:47 +0300

    add test for enhancedTableRow

---
+++ src/tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
@@ -0,0 +1,112 @@
+import { describe } from 'vitest'
+import { vi } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import EnhancedTableRow from '~/components/enhanced-table/enhanced-table-row/EnhancedTableRow'
+
+describe('EnhancedTableRow', () => {
+  const mockHandleSelectClick = vi.fn()
+  const mockRefetch = vi.fn()
+  const mockAction = vi.fn()
+  const rowData = { _id: '1', name: 'Jhon', price: '20' }
+
+  const baseProps = {
+    columns: [
+      { field: 'name', label: 'Name' },
+      { field: 'price', label: 'Price' }
+    ],
+    isSelection: true,
+    item: rowData,
+    refetchData: mockRefetch,
+    rowActions: [{ label: 'Edit', func: mockAction }],
+    select: {
+      isSelected: vi.fn().mockReturnValue(false),
+      handleSelectClick: mockHandleSelectClick
+    },
+    selectedRows: []
+  }
+
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should render table row with correct data', () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...baseProps} />
+        </tbody>
+      </table>
+    )
+
+    expect(screen.getByText('Wine A')).toBeInTheDocument()
+    expect(screen.getByText('20')).toBeInTheDocument()
+  })
+
+  it('should call handleSelectClick when checkbox is clicked', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...baseProps} />
+        </tbody>
+      </table>
+    )
+
+    const checkbox = screen.getByRole('checkbox')
+    await userEvent.click(checkbox)
+    expect(mockHandleSelectClick).toHaveBeenCalled()
+    expect(mockHandleSelectClick).toHaveBeenCalledWith(expect.anything(), '1')
+  })
+
+  it('should render action menu when menu icon is clicked', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...baseProps} />
+        </tbody>
+      </table>
+    )
+
+    const menuIcon = screen.getByTestId('menu-icon')
+    await userEvent.click(menuIcon)
+
+    expect(screen.getByRole('menu')).toBeInTheDocument()
+    expect(screen.getByText('Edit')).toBeInTheDocument()
+  })
+
+  it('should call onAction function when clicking on the menu item', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...baseProps} />
+        </tbody>
+      </table>
+    )
+
+    const menuIcon = screen.getByTestId('menu-icon')
+    await userEvent.click(menuIcon)
+
+    const menuItem = screen.getByText('Edit')
+    await userEvent.click(menuItem)
+
+    expect(mockAction).toHaveBeenCalledWith('1')
+    expect(mockRefetch).toHaveBeenCalled()
+  })
+
+  it('should close menu when "escape" is pressed', async () => {
+    render(
+      <table>
+        <tbody>
+          <EnhancedTableRow {...baseProps} />
+        </tbody>
+      </table>
+    )
+
+    const menuIcon = screen.getByTestId('menu-icon')
+    await userEvent.click(menuIcon)
+
+    expect(screen.getByRole('menu')).toBeInTheDocument()
+
+    fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' })
+  })
+})
